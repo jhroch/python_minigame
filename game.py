beffb8e92f0ed1ef2b75b97d73ca583b7eef6663
Jiří Hroch <jirihroch@icloud.com>
2025-05-29 17:28:05 +0200

    First commit

---
+++ game.py
@@ -0,0 +1,70 @@
+import curses
+import random
+
+size: int = 8
+
+class Player:
+    def __init__(self, x, y, points):
+        self.x = x
+        self.y = y
+        self.points = points
+    def up(self):
+        if(0 <= self.y - 1 < size):
+            self.y = self.y-1
+    def down(self):
+        if(0 <= self.y + 1 < size):
+            self.y = self.y+1
+    def left(self):
+        if(0 <= self.x - 1 < size):
+            self.x = self.x-1
+    def right(self):
+        if(0 <= self.x + 1 < size):
+            self.x = self.x+1
+    def inc(self):
+        self.points = self.points+1
+
+class Korist:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+player = Player(0, 0, 0)
+korist = Korist(random.randint(0, size-1), random.randint(0, size-1))
+
+def printGame(stdscr) -> None:
+    for i in range(size):
+        for j in range(size):
+            stdscr.addstr("+-")
+        stdscr.addstr("+\n")
+        for k in range(size):
+            if(i == player.y and k == player.x): stdscr.addstr("|*")
+            elif(i == korist.y and k == korist.x): stdscr.addstr("|&")
+            else: stdscr.addstr("| ")
+        stdscr.addstr("|\n")
+    for j in range(size):
+        stdscr.addstr("+-")
+    stdscr.addstr("+\n",)
+    stdscr.addstr(f"Points: {player.points}\n")
+
+
+def main(stdscr):
+    curses.curs_set(0) # Skryje kurzor
+    while(1):
+        stdscr.clear()
+        if(player.x == korist.x and player.y == korist.y):
+            player.inc()
+            korist.x = random.randint(0, size-1)
+            korist.y = random.randint(0, size-1)
+        printGame(stdscr)
+        stdscr.refresh()
+        key = stdscr.getch()
+        if(key == ord('q')): break
+        if(key == curses.KEY_UP): player.up()
+        if(key == curses.KEY_DOWN): player.down()
+        if(key == curses.KEY_LEFT): player.left()
+        if(key == curses.KEY_RIGHT): player.right()
+        
+        else: continue
+
+curses.wrapper(main)
+        
